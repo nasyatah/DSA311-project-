ee8af18fc17f6656e1b7a8eb17e09972e918a61d
lowtingyu <lowtingyu.lty@gmail.com>
2024-03-07 15:07:23 +0800

    updated excel

--- findClosestEntities.py
+++ findClosestEntities.py
@@ -14,22 +14,22 @@ mall_df.columns = ["name", "lat", "long"]
 
 tqdm.tqdm.pandas()
 
-closestBeaches = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, beach_df), axis = 1)
-closestBeaches = pd.DataFrame(list(closestBeaches))
-closestBeaches.columns = ["closestBeach", "distToBeach", "timeToBeach", "beachesIn10km"]
-hdbCoordinates.join(closestBeaches).to_csv("./data/closestBeaches.csv")
+##closestBeaches = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, beach_df), axis = 1)
+##closestBeaches = pd.DataFrame(list(closestBeaches))
+##closestBeaches.columns = ["closestBeach", "distToBeach", "timeToBeach", "beachesIn10km"]
+##hdbCoordinates.join(closestBeaches).to_csv("./data/closestBeaches.csv")
+##
+##closestMalls = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, mall_df), axis = 1)
+##closestMalls = pd.DataFrame(list(closestMalls))
+##closestMalls.columns = ["closestMall", "distToMall", "timeToMall", "mallsIn10km"]
+##hdbCoordinates.join(closestMalls).to_csv("./data/closestMalls.csv")
+##
+##closestPrimarySchs = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, sch_df), axis = 1)
+##closestPrimarySchs = pd.DataFrame(list(closestPrimarySchs))
+##closestPrimarySchs.columns = ["closestPrimarySch", "distToPrimarySch", "timeToPrimarySch", "PrimarySchsIn10km"]
+##hdbCoordinates.join(closestPrimarySchs).to_csv("./data/closestPrimarySchs.csv")
 
-closestMalls = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, mall_df), axis = 1)
-closestMalls = pd.DataFrame(list(closestMalls))
-closestMalls.columns = ["closestMall", "distToMall", "timeToMall", "mallsIn10km"]
-hdbCoordinates.join(closestMalls).to_csv("./data/closestMalls.csv")
-
-closestPrimarySchs = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, sch_df), axis = 1)
-closestPrimarySchs = pd.DataFrame(list(closestPrimarySchs))
-closestPrimarySchs.columns = ["closestPrimarySch", "distToPrimarySch", "timeToPrimarySch", "PrimarySchsIn10km"]
-hdbCoordinates.join(closestPrimarySchs).to_csv("./data/closestPrimarySchs.csv")
-
-closestMRTStations = hdbCoordinates[["lat", "long"]].progress_apply(lambda x: findClosestEntity(x.values, trans_df, dist_type = "walk"), axis = 1)
+closestMRTStations = hdbCoordinates[["lat", "long"]].iloc[1:20].progress_apply(lambda x: findClosestEntity(x.values, trans_df, dist_type = "walk"), axis = 1)
 closestMRTStations = pd.DataFrame(list(closestMRTStations))
 closestMRTStations.columns = ["closestMRTStation", "distToMRTStation", "timeToMRTStation", "MRTStationsIn10km"]
-hdbCoordinates.join(closestMRTStations).to_csv("./data/closestMRTStations.csv")
+hdbCoordinates.join(closestMRTStations).to_csv("./data/closestMRTStations.test2.csv")
